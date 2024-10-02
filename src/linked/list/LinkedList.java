ddd9e474a068c7182d09ac4f62485c4fab4ec562
Egon Nguyen <d.h.e.nguyen@student.hhs.nl>
2024-10-02 21:47:59 +0200

    added code to methods: getFirst, getLast, getSize, get(index) and addFirst. Tests were successful

---
+++ src/linked/list/LinkedList.java
@@ -0,0 +1,182 @@
+package linked.list;
+
+/**
+ * This class is supposed to represent a doubly linked list
+ * Finish the methods below
+ * The tests can guide you in the right direction but are not absolute infallible
+ */
+public class LinkedList implements OOSELinkedList {
+    /***
+     * Leave these three variables.
+     */
+    private LinkedListNode<Double> head;
+    private LinkedListNode<Double> tail;
+    private Integer size = 0;
+
+    /**
+     * @return The value of the head of the linked list.
+     * Checks if head is null. If it is, return null.
+     */
+    @Override
+    public Double getFirst() {
+        if (head == null){
+            return null;
+        }
+        else{
+            return head.getValue();
+        }
+    }
+
+    /**
+     * @return The value of the tail of the linked list.
+     * Checks if tail is null. If it is, return null.
+     */
+    @Override
+    public Double getLast() {
+        if (tail == null){
+            return null;
+        }
+        else{
+            return tail.getValue();
+        }
+    }
+
+    /**
+     * @return The size of the linked list
+     */
+    @Override
+    public Integer getSize() {
+        return size;
+    }
+
+    /**
+     * @param index
+     * @return An element of the LinkedList based on an index.
+     * If index is not present (List to small or negative index), throw IndexOutOfBoundsException
+     * STUDENTS: implementing the exception is optional. You are allowed to return null a test will fail.
+     */
+    @Override
+    public Double get(int index) {
+        LinkedListNode<Double> currentNode = head;
+
+        if(index > size){
+            throw new IndexOutOfBoundsException("index is bigger than size of the list");
+        }
+        else{
+
+            for (int i = 0; i < index ; i++){
+                currentNode = currentNode.getNext();
+            }
+            return currentNode.getValue();
+        }
+    }
+
+    /**
+     * @param d Adds an element to head of the linked list and increases the size.
+     *          In case the head is already filled, this method also updates the previous/next node attributes.
+     *          In case this is the element added, also fill the tail.
+     */
+    @Override
+    public void addFirst(Double d) {
+        LinkedListNode<Double> newNode = new LinkedListNode<>(d);
+        if(size > 0){
+            //move head to second place
+            head.setPrevious(newNode);
+        }
+        //set new head
+        head = newNode;
+    }
+
+    /**
+     * @param d Adds an element to tail of the linked list and increases the size.
+     *          In case the tail is already filled, this method also updates the previous/next node attributes.
+     *          In case this is the element added, also fill the head.
+     */
+    @Override
+    public void addLast(Double d) {
+    }
+
+    /**
+     * @param index
+     * @param d     Adds an element to the linked list based on an index
+     *              In case index out of bounds, throw and IndexOutOfBoundsException (optional but a test will fail if you don't)
+     *              Updates the surrounding nodes' previous and next variables
+     *              Also updates the size
+     *              <p>
+     *              TIP: be smart with how you 'move' to the correct element.
+     *              Mind the time complexity - O(?)
+     */
+    @Override
+    public void add(int index, Double d) {
+    }
+
+    /**
+     * Removes the head of the linked list.
+     * If there is only one element - also remove tail
+     * Otherwise it updates the next elements previous/next node variables
+     * Also updates the size
+     */
+    @Override
+    public void removeFirst() {
+    }
+
+    /**
+     * Removes the tail of the linked list.
+     * If there is only one element - also remove head
+     * Otherwise it updates the previous elements previous/next node variables
+     * Also updates the size
+     */
+    @Override
+    public void removeLast() {
+    }
+
+    /**
+     * @param index Removes an element based on an index.
+     *              In case index out of bounds, throw and IndexOutOfBoundsException (optional but a test will fail if you don't)
+     *              Updates the surrounding nodes' previous and next variables
+     *              Also updates the size
+     */
+    @Override
+    public void remove(int index) {
+
+    }
+
+
+    /**
+     * Sets the value of an element based on an index.
+     * In case index out of bounds, throw and IndexOutOfBoundsException (optional but a test will fail if you don't)
+     *
+     * @param index
+     * @param value
+     */
+    @Override
+    public void set(int index, Double value) {
+    }
+
+
+    /**
+     * Sorts the list in ascending order.
+     * You can use any existing sorting algorithm like Bubble sort, Merge sort, quick sort etc.
+     * Or you can create your own...
+     * You cannot use build in sort methods like Collection.sort() or Arrays.sort()
+     * The sorting algorithm should be a different from the one used for the Stack.
+     */
+    public static OOSELinkedList sort(OOSELinkedList list) {
+        return null;
+
+    }
+
+    /*
+     * OPTIONAL:
+     *  Make the most out of the doubly linked list by determining whether to start from the head or from the tail
+     *  Can be interesting for the Add(), Get() and Remove() methods
+     *
+     *  Also, optional: implement the following methods:
+     *  Clear()
+     *  Contains()
+     *  IndexOf()
+     * */
+
+}
+
+
